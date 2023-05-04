eda7bddc286e39ba29b44db77621765d0cf0708a
KeshaPatel7 <keshapatel@github.com>
2023-05-04 17:42:21 -0400

    diabetesDf code uploaded

--- diabetesDF.py
+++ diabetesDF.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import pickle
+from io import BytesIO
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import boto3
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+df = pd.read_csv('https://diabetesprojectfinal.s3.amazonaws.com/dataset/diabetes_prediction_dataset.csv')
+
+plt.figure(figsize=(10,5))
+sns.heatmap(df.corr(),annot=True,cmap='coolwarm')
+
+# Save the processed data to an S3 bucket
+s3 = boto3.resource('s3', region_name='us-east-1')
+bucket_name = 'diabetesprojectfinal'
+
+# Save the plot as a PNG file in memory
+png_buffer = BytesIO()
+plt.savefig(png_buffer, format='png')
+png_buffer.seek(0)
+
+# Upload the plot to S3 as an object
+object_key = f'plot/heatmap.png'
+s3.Bucket(bucket_name).put_object(Key=object_key, Body=png_buffer)
+s3.ObjectAcl(bucket_name, object_key).put(ACL='public-read')
+
+# Close the plot to free up memory
+plt.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#Machine learning model
+encoder = OneHotEncoder(drop='first')
+X_cat = encoder.fit_transform(df[['gender', 'smoking_history']])
+X_num = df[['age', 'hypertension', 'heart_disease', 'bmi', 'HbA1c_level', 'blood_glucose_level']]
+X = pd.concat([pd.DataFrame(X_cat.toarray()), X_num], axis=1)
+y = df['diabetes']
 
+X.columns = X.columns.astype(str)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+accuracy = model.score(X_test, y_test)
+print('Accuracy:', accuracy)
+
+with open('diabetes_model.pkl', 'wb') as f:
+    pickle.dump(model,f)
+#Uploading pickle file to s3
+s3 = boto3.client('s3')
+bucket_name = 'diabetesprojectfinal'
+key_name = 'diabetes_model.pkl'
+model_pickle = pickle.dumps(model)
+response = s3.put_object(Bucket=bucket_name, Key=key_name, Body=model_pickle)

2f6c93b2caf7387521aca4315b8880ba18531d95
trand <kieutrungtran@gmail.com>
2023-05-05 16:12:47 -0400

    adjust port

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import pickle
 
 app = Flask(__name__)
 
-s3 = boto3.client('s3')
-# Download the file from S3
-bucket_name = 'diabetesprojectfinal'
-s3.download_file(bucket_name, 'model/heart_attack_model.pkl', 'model/heart_attack_model.pkl')
-s3.download_file(bucket_name, 'model/diabetes_model.pkl', 'model/diabetes_model.pkl')
+# s3 = boto3.client('s3')
+# # Download the file from S3
+# bucket_name = 'diabetesprojectfinal'
+# s3.download_file(bucket_name, 'model/heart_attack_model.pkl', 'model/heart_attack_model.pkl')
+# s3.download_file(bucket_name, 'model/diabetes_model.pkl', 'model/diabetes_model.pkl')
 
 @app.route('/')
 def index():
@@ -33,8 +33,8 @@ def diabetes_results():
     hypertension = int(request.form['hypertension'])
     heart_disease = int(request.form['heart_disease'])
     smoking_history = int(request.form['smoking_history'])
-    bmi = int(request.form['bmi'])
-    hba1c_level = int(request.form['HbA1c_level'])
+    bmi = float(request.form['bmi'])
+    hba1c_level = float(request.form['HbA1c_level'])
     blood_glucose_level = int(request.form['blood_glucose_level'])
 
     form_data = np.array([[gender, age, hypertension, heart_disease, smoking_history, bmi, hba1c_level,
@@ -50,8 +50,8 @@ def diabetes_results():
 def heart_attack():
     return render_template('heart_attack.html')
 
-with open('model/heart_attack_model.pkl', 'rb') as file:
-    model2 = pickle.load(file)
+# with open('model/heart_attack_model.pkl', 'rb') as file:
+#     model2 = pickle.load(file)
 
 @app.route('/hear_attack_results', methods=['POST'])
 def heart_attack_results():
